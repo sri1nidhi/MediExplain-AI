085d254058727c58c13ab360a1070f1c67ab5b35
sri_1_nidhi <sri788nidhi@gmail.com>
2026-06-11 13:29:14 +0530

    fix deplyoment

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from dotenv import load_dotenv
+
 from utils.pdf_loader import extract_pdf_text
 from utils.llm import get_llm
 
-load_dotenv()
+
 
 st.set_page_config(
     page_title="MediExplain AI",
--- utils/llm.py
+++ utils/llm.py
@@ -1,10 +1,10 @@
-import os
+import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 def get_llm():
 
     return ChatGoogleGenerativeAI(
         model="gemini-2.5-flash",
-        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        google_api_key=st.secrets["GOOGLE_API_KEY"],
         temperature=0.2
     )
